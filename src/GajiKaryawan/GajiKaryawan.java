a672eeccb5e792268f4adc6983bacd70890c6439
Sm.Ardi <akucakep994@gmail.com>
2026-05-19 13:23:21 +0700

    First commit

--- src/GajiKaryawan/GajiKaryawan.java
+++ src/GajiKaryawan/GajiKaryawan.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GajiKaryawan {
 
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -14,7 +15,7 @@ public class GajiKaryawan {
         double gajiLembur;
         double totalGaji;
 
-        // Input
+        //Input
         System.out.print("Masukkan Golongan (A/B/C): ");
         golongan = input.nextLine();
 
@@ -24,37 +25,39 @@ public class GajiKaryawan {
         // Gaji Pokok
         if (golongan.equalsIgnoreCase("A")) {
             gajiPokok = 5000000;
-        } else if (golongan.equalsIgnoreCase("B")) {
+           }   else if (golongan.equalsIgnoreCase("B")) {
             gajiPokok = 6500000;
-        } else if (golongan.equalsIgnoreCase("C")) {
+           }    else if (golongan.equalsIgnoreCase("C")) {
             gajiPokok = 9500000;
-        } else {
+           }    else {
             System.out.println("Golongan tidak valid!");
             input.close();
             return;
-        }
-
+           }
+            
         // Lembur
-        if (jamLembur == 1) {
+        if (jamLembur == 1){
             persen = 0.30;
         } else if (jamLembur == 2) {
             persen = 0.32;
         } else if (jamLembur == 3) {
             persen = 0.34;
-        } else if (jamLembur >= 4) {
+        }else if (jamLembur == 4) {
             persen = 0.36;
+        } else if (jamLembur >= 5) {
+            persen = 0.38;
         }
 
         // Hitung
         gajiLembur = gajiPokok * persen;
         totalGaji = gajiPokok + gajiLembur;
 
-        // Output
+        //Output
         System.out.println("\n=== HASIL ===");
-        System.out.println("Golongan   : " + golongan.toUpperCase());
-        System.out.println("Gaji Pokok : Rp " + gajiPokok);
-        System.out.println("Lembur     : Rp " + gajiLembur);
-        System.out.println("Total Gaji : Rp " + totalGaji);
+        System.out.println("Golongan    : " + golongan.toUpperCase());
+        System.out.println("Gaji pokok : Rp " + gajiPokok);
+        System.out.println("Lembur    : Rp " + gajiPokok);
+        System.out.println("Total Gaji   : Rp " + totalGaji);
 
         input.close();
     }
